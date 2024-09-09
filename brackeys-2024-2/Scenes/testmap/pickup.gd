46f9ee5650fcef20cbade34b6d8d9c0653c4b675
carewarez <git@careware.sh>
2024-09-09 15:09:40 -0700

    Disable pickup texture and collider on collision with respawn timer

--- brackeys-2024-2/Scenes/testmap/pickup.gd
+++ brackeys-2024-2/Scenes/testmap/pickup.gd
@@ -1,6 +1,19 @@
 class_name Pickup
 
 extends Area2D
+# Initialize timer for tracking time between
+@onready var timer: Timer = $Timer
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
-	print("Body entered!")
+	
+	if collision_shape_2d.disabled == false:
+		print("Body entered!")
+		visible = false
+		collision_shape_2d.disabled = true	
+		timer.start()
+
+
+func _on_timer_timeout() -> void:
+	visible = true
+	collision_shape_2d.disabled = false
