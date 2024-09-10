148e729241bd602af561ae6d5ab9718e832e97b9
Ikkiren <chris_dupuis@outlook.com>
2024-09-10 16:23:35 +0200

    refactor/TypeScripting
    added types + some protections

--- brackeys-2024-2/Scenes/state_machine/chasing_state.gd
+++ brackeys-2024-2/Scenes/state_machine/chasing_state.gd
@@ -5,14 +5,13 @@ var player: CharacterBody2D
 
 var size = 128
 
-
 class Point:
-	var point
-	var close_point
-	var check_points
-	var g = 0
-	var h = 0
-	var connection = 0
+	var point: Vector2
+	var close_point: Array
+	var check_points: Array
+	var g: int = 0
+	var h: int = 0
+	var connection: Point = null
 	
 	func _init(x, y):
 		point = Vector2(x, y)
@@ -25,56 +24,56 @@ class Point:
 var start_point = Vector2.ZERO
 var target_point = Vector2.ZERO
 
-func get_h(point):
+func get_h(point: Vector2) -> int:
 	return abs(point.x - target_point.point.x) + abs(point.y - target_point.point.y)
 	
-func find_point(point_array, point):
+func find_point(point_array: Array, point: Point) -> int:
 	for i in range(point_array.size()):
 		if (point_array[i].point == point.point):
 			return i
 	return -1
 
-func a_star():
+func a_star() -> Array:
 	var open_array = [start_point]
 	var close_array = []
-	while (open_array.size() > 0):
-		var current_point = open_array[0]
+	
+	while open_array.size() > 0:
+		var current_point: Point = open_array[0]
 		for i in range(open_array.size()):
 			if (current_point.f() > open_array[i].f() || (current_point.f() == open_array[i].f() && current_point.h > open_array[i].h)):
 				current_point = open_array[i]
+		
 		close_array.push_back(current_point)
 		open_array.erase(current_point)
-		if (current_point.point.x == target_point.point.x && current_point.point.y == target_point.point.y):
-			var current_point_path = current_point
+		if current_point.point.x == target_point.point.x && current_point.point.y == target_point.point.y:
+			var current_point_path: Point = current_point
 			var path = []
-			while (current_point_path.point.x != start_point.point.x || current_point_path.point.y != start_point.point.y):
+			while current_point_path.point.x != start_point.point.x || current_point_path.point.y != start_point.point.y:
 				path.push_back(current_point_path)
 				current_point_path = current_point_path.connection
 			return path
-		var i = 0
-		while (i < current_point.close_point.size()):
-			var close_point = Point.new(current_point.close_point[i].x, current_point.close_point[i].y)
-			print(current_point.point)
-			print(enemy.global_transform)
-			print(enemy.test_move(enemy.global_transform, current_point.check_points[i]))
-			if (find_point(close_array, close_point) != -1 || enemy.test_move(Transform2D(Vector2(1,0), Vector2(0,1), current_point.point), current_point.check_points[i])):
-				i += 1
+			
+		for i in range(current_point.close_point.size()):
+			var close_point: Point = Point.new(current_point.close_point[i].x, current_point.close_point[i].y)
+			if find_point(close_array, close_point) != -1 || enemy.test_move(Transform2D(Vector2(1,0), Vector2(0,1), current_point.point), current_point.check_points[i]):
+				if close_point.point + current_point.check_points[i] == target_point.point:
+					return []
 				continue
-			var cost_next = current_point.g + 1
-			if (find_point(open_array, close_point) == -1 || cost_next < open_array[find_point(open_array, close_point)].g):
+			var cost_next: int = current_point.g + 1
+			
+			if find_point(open_array, close_point) == -1 || cost_next < open_array[find_point(open_array, close_point)].g:
 				close_point.g = cost_next
 				close_point.connection = current_point
-				if (find_point(open_array, close_point) == -1):
-					close_point.h = get_h(close_point.point)
-					open_array.push_back(close_point)
-			i += 1
+				close_point.h = get_h(close_point.point)
+				open_array.push_back(close_point)
+					
 	return []
 
 
 func Move():
 	start_point = Point.new(enemy.position.x, enemy.position.y)
 	target_point = Point.new(player.position.x, player.position.y)
-	var new_position = a_star()
+	var new_position: Array = a_star()
 	if (new_position.size() > 0):
 		new_position.reverse()
 		enemy.position = new_position[0].point
