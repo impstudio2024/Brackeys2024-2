ddd7d421f3a58eacbdcc23fcdbc075c9c83c761b
gabpjat <gabpjat@gmail.com>
2024-09-10 15:30:03 -0300

    add simple weapon switching and display

--- brackeys-2024-2/Scenes/testmap/testcharacter.gd
+++ brackeys-2024-2/Scenes/testmap/testcharacter.gd
@@ -3,14 +3,16 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
 @onready var weapon_displayed = $WeaponDisplayed
+enum Weapons { NONE, BROADSWORD, SPEAR, BOW }
+@export var current_weapon : Weapons
+
+func _ready() -> void:	
+	set_meta("type","player")
+	display_weapon(Weapons.NONE)
 
 func _physics_process(delta):
-
-	#Get the input direction: -1 0 1
-	var horizontal_direction = Input.get_axis("move_left", "move_right")
-	var vertical_direction = Input.get_axis("move_up", "move_down")
-	
 	#Apply movement
 	if Input.is_action_just_pressed("move_up"):
 		position.y -= 16
@@ -26,6 +28,8 @@ func _physics_process(delta):
 
 	move_and_slide()
 	
-func display_weapon(sprite: AnimatedSprite2D):
-	weapon_displayed = sprite 
+func display_weapon(weapon: Weapons):
+	#0 -> no weapon | 1 -> broadsword | 2 -> spear | 3 -> bow
+	weapon_displayed.set_frame_and_progress(weapon, 0.0)
+	 
 	
