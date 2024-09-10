ea22aa088d20367cf96d85f3e7ac7ca3dca9239b
Ivan Diaz <impstudio2024@gmail.com>
2024-09-10 21:00:14 +0200

    FIX:  remapped control names

--- brackeys-2024-2/scripts/characters.gd
+++ brackeys-2024-2/scripts/characters.gd
@@ -4,18 +4,18 @@ class_name Character
 var map_position: Vector2i
 
 func _ready() -> void:
-    add_to_group('character')
-    map_position = Global.entities.local_to_map(position)
+	add_to_group('character')
+	map_position = Global.entities.local_to_map(position)
 
 func move(relative_movement: Vector2i) -> Character:
-    if Global.entities.get_cell_source_id(map_position + relative_movement):
-        for character in get_tree().get_nodes_in_group('character'):
-            if not character.map_position == map_position + relative_movement: continue
-            return character as Character
+	if Global.entities.get_cell_source_id(map_position + relative_movement):
+		for character in get_tree().get_nodes_in_group('character'):
+			if not character.map_position == map_position + relative_movement: continue
+			return character as Character
 
 
-    # move the character
-    position = Global.entities.map_to_local(map_position + relative_movement)
-    # when moving the map position will also need to be updated
-    map_position = Global.entities.local_to_map(position)
-    return null
+	# move the character
+	position = Global.entities.map_to_local(map_position + relative_movement)
+	# when moving the map position will also need to be updated
+	map_position = Global.entities.local_to_map(position)
+	return null
--- brackeys-2024-2/scripts/player.gd
+++ brackeys-2024-2/scripts/player.gd
@@ -3,26 +3,26 @@ extends Character
 var turn_active: bool = true
 
 func _process(_delta: float) -> void:
-    # placeholder
-    if Input.is_action_just_pressed("space"):
-        turn_active = true
+	# placeholder
+	if Input.is_action_just_pressed("space"):
+		turn_active = true
 
 
-    if not turn_active: return
+	if not turn_active: return
 
-    var movement: Vector2i
-    if Input.is_action_pressed("down"): movement.y = 1
-    elif Input.is_action_pressed('up'): movement.y = -1
+	var movement: Vector2i
+	if Input.is_action_pressed("move_down"): movement.y = 1
+	elif Input.is_action_pressed('move_up'): movement.y = -1
 
-    if Input.is_action_pressed("left"): movement.x = -1
-    elif Input.is_action_pressed('right'): movement.x = 1
+	if Input.is_action_pressed("move_left"): movement.x = -1
+	elif Input.is_action_pressed('move_right'): movement.x = 1
 
-    if Input.is_action_pressed('down') and Input.is_action_pressed('up'): movement.y = 0
-    if Input.is_action_pressed('left') and Input.is_action_pressed('right'): movement.x = 0
+	if Input.is_action_pressed('move_down') and Input.is_action_pressed('move_up'): movement.y = 0
+	if Input.is_action_pressed('move_left') and Input.is_action_pressed('move_right'): movement.x = 0
 
-    if movement.length_squared() > 1:
-        movement = Vector2i.ZERO
+	if movement.length_squared() > 1:
+		movement = Vector2i.ZERO
 
-    if movement != Vector2i.ZERO:
-        turn_active = false
-        print(move(movement))
+	if movement != Vector2i.ZERO:
+		turn_active = false
+		print(move(movement))
