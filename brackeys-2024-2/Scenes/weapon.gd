8275735216af1aa33e8d591ba01d5eb3f7e93843
gabpjat <gabpjat@gmail.com>
2024-09-10 15:32:41 -0300

    display weapon on pickup

--- brackeys-2024-2/Scenes/weapon.gd
+++ brackeys-2024-2/Scenes/weapon.gd
@@ -1,7 +1,8 @@
 class_name Weapon
 extends Pickup
 
-@onready var weapon_sprite = $WeaponSprite
+enum Weapons { NONE, BROADSWORD, SPEAR, BOW }
+@export var type: Weapons
 
 @export var damage: int
 #@export var radius: int
@@ -13,17 +14,19 @@ func _ready():
 
 func _on_body_entered(body: Node2D) -> void:
 	
-	if collision_shape_2d.disabled == false:
-		print("Weapon picked up!")
-		
-		#call the method for changing the player's weapon sprite
-		
-		
-		#visible = false
-		#collision_shape_2d.disabled = true	
-		## Initialize timer for respawning the item (optional)
-		#if expirable:
-			#timer.start()
+	if body.has_meta("type"):
+		if collision_shape_2d.disabled == false:
+			print(Weapons.keys()[type] + " picked up!")
+			
+			
+			#call the method for changing the player's weapon sprite
+			body.display_weapon(type)
+			
+			#visible = false
+			#collision_shape_2d.disabled = true	
+			## Initialize timer for respawning the item (optional)
+			#if expirable:
+				#timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
