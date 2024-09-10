6bda56b9417422233f0abc614fa6f35579e4b243
carewarez <git@careware.sh>
2024-09-10 16:27:44 -0700

    Define test function for enemy movement

--- brackeys-2024-2/scripts/enemy.gd
+++ brackeys-2024-2/scripts/enemy.gd
@@ -3,3 +3,6 @@ extends Character
 func _ready() -> void:
 	Global.connect("character_moved",onPlayerMove) # Connect character_moved signal so that enemies will move after the player has moved
 
+# Move the enemy character up 1 unit when the player has moved
+func onPlayerMove():
+	move(Vector2i.UP)
