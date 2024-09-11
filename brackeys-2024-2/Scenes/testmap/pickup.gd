386c4f504926df64dbe077b841df677bb49f2df3
gabpjat <gabpjat@gmail.com>
2024-09-10 22:56:54 -0300

    add check to prevent enemies from picking up items

--- brackeys-2024-2/Scenes/testmap/pickup.gd
+++ brackeys-2024-2/Scenes/testmap/pickup.gd
@@ -6,14 +6,14 @@ extends Area2D
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 func _on_body_entered(body: Node2D) -> void:
-	
-	if collision_shape_2d.disabled == false:
-		print("Body entered!")
-		visible = false
-		collision_shape_2d.disabled = true	
-		# Initialize timer for respawning the item (optional)
-		if renewable:
-			timer.start()
+	if body.is_in_group("player"):
+		if collision_shape_2d.disabled == false:
+			print("Body entered!")
+			visible = false
+			collision_shape_2d.disabled = true	
+			# Initialize timer for respawning the item (optional)
+			if renewable:
+				timer.start()
 
 
 func _on_timer_timeout() -> void:
