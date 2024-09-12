634015ef3e40534d5e822f60331deeb32b6d0469
Dávid Groniewsky <99834433+gro-david@users.noreply.github.com>
2024-09-12 06:59:34 +0200

    STILL TRYING TO FIX

--- brackeys-2024-2/scripts/gm.gd
+++ brackeys-2024-2/scripts/gm.gd
@@ -1,10 +1,20 @@
 extends Node2D
 
+var enemies: Array[Enemy] = []
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var state_machine = $StateMachine
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	print('ready')
+	Global.enemy_added.connect(_on_enemy_added)
+	Global.enemies_turn.connect(enemies_turn.bind())
+	
+func _on_enemy_added(enemy: Enemy):
+	enemies.append(enemy)
+	print(enemy)
+	
+func enemies_turn():
+	print('enemies turn')
+	for enemy in enemies:
+		enemy.on_enemy_turn()
+		print(enemy)
+	
+	Global.players_turn.emit()
