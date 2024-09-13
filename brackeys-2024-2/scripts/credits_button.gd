65e079f51f468825d873358c8c56627a19a321be
carewarez <git@careware.sh>
2024-09-13 14:22:50 -0700

    Add team member names to list in credits_button script

--- brackeys-2024-2/scripts/credits_button.gd
+++ brackeys-2024-2/scripts/credits_button.gd
@@ -1,6 +1,36 @@
 extends Button
 
-
+# Create a list of credits to be displayed
+var credits = [
+	
+	["A Game by ImpStudio"],
+	
+	["naxiv"],
+	
+	["Dex"],
+	
+	["Malario"],
+	
+	["Ikkiren"],
+	
+	["kierownik"],
+	
+	["Kyoresu"],
+	
+	["Mapet"],
+	
+	["JackleBox"],
+	
+	["sx0rm"],
+	
+	["WeBack"],
+	
+	["careware"],
+	
+	["Special thanks to"],
+		["Brackeys"],
+		["Godot"],
+	]
 
 
 func _on_pressed() -> void:
