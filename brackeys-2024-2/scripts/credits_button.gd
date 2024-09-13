39e10cc0d27a303b57e87c4c0ccb7c2cb1316ee2
carewarez <git@careware.sh>
2024-09-13 15:17:02 -0700

    Remove list of names from CreditsButton

--- brackeys-2024-2/scripts/credits_button.gd
+++ brackeys-2024-2/scripts/credits_button.gd
@@ -1,38 +1,5 @@
 extends Button
 
-# Create a list of credits to be displayed
-var credits = [
-	
-	["A Game by ImpStudio"],
-	
-	["naxiv"],
-	
-	["Dex"],
-	
-	["Malario"],
-	
-	["Ikkiren"],
-	
-	["kierownik"],
-	
-	["Kyoresu"],
-	
-	["Mapet"],
-	
-	["JackleBox"],
-	
-	["sx0rm"],
-	
-	["WeBack"],
-	
-	["careware"],
-	
-	["Special thanks to"],
-		["Brackeys"],
-		["Godot"],
-	]
-
-
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 	
