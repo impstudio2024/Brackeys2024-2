592bc36e80a5782cba29f91083b89aca6b29cf5d
gabpjat <gabpjat@gmail.com>
2024-09-14 00:54:28 -0300

    add health, damaging and death for characters

--- brackeys-2024-2/scripts/characters.gd
+++ brackeys-2024-2/scripts/characters.gd
@@ -2,6 +2,8 @@ extends CharacterBody2D
 
 class_name Character
 
+@onready var health : int = 15
+@onready var damage : int = 5
 
 var map_position: Vector2i
 
@@ -32,3 +34,18 @@ func move(relative_movement: Vector2i) -> Character:
 	# when moving the map position will also need to be updated
 	map_position = Global.entities.local_to_map(position)
 	return null
+	
+func damage_by(damage: int):
+	health -= damage
+	#TODO: play animation, maybe a simple red color modulation for a few frames
+	if health <= 0:
+		send_to_the_backrooms()
+	
+func send_to_the_backrooms():
+	if self.is_in_group("enemies"):
+		print("enemy killed")
+		#self.state
+		Global.enemy_killed.emit() #
+	if self.is_in_group("player"):
+		Global.game_over.emit()
+	
