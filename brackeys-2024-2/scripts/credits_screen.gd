0e2611482b44cbb79928a5610cf3e7f820f544e6
carewarez <git@careware.sh>
2024-09-13 18:33:56 -0700

    Get credits screen background scrolling on the y-axis in credits_screen script

--- brackeys-2024-2/scripts/credits_screen.gd
+++ brackeys-2024-2/scripts/credits_screen.gd
@@ -1,1 +1,11 @@
 extends Control
+
+@onready var parallax_layer: ParallaxLayer = $ParallaxBackground/ParallaxLayer
+
+@export var scroll_speed: int = 50
+
+# First attempt at scrolling parallax layer
+func _process(delta: float) -> void:
+	parallax_layer.motion_offset.y -= scroll_speed * delta
+	
+	
