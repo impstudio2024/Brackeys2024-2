97db8412903b23ee0d646e165738cf3b9d169934
gabpjat <gabpjat@gmail.com>
2024-09-14 15:35:34 -0300

    credits screen parallax fix

--- brackeys-2024-2/scripts/credits_screen.gd
+++ brackeys-2024-2/scripts/credits_screen.gd
@@ -7,7 +7,7 @@ extends Control
 
 # First attempt at scrolling parallax layer
 func _process(delta: float) -> void:
-	parallax_bg_layer.motion_offset.y -= scroll_speed * delta
+	parallax_bg_layer.motion_offset.y -= 40 * delta
 	parallax_text_layer.motion_offset.y -= scroll_speed * delta
 	#position.x = 0
 	
