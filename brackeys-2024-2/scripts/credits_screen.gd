ce28e535ed5ddaa823a3980e882fc440b9b20569
carewarez <git@careware.sh>
2024-09-14 11:43:47 -0700

    Start timer on _ready() and return to main menu _on_timer_timeout()

--- brackeys-2024-2/scripts/credits_screen.gd
+++ brackeys-2024-2/scripts/credits_screen.gd
@@ -6,8 +6,12 @@ extends Control
 
 @export var scroll_speed: int = 50
 
+func _ready():
+	timer.start()
 # First attempt at scrolling parallax layer
 func _process(delta: float) -> void:
 	parallax_bg_layer.motion_offset.y -= 40 * delta
 	parallax_text_layer.motion_offset.y -= scroll_speed * delta
 	
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file("res://Scenes/mainmenu.tscn")
