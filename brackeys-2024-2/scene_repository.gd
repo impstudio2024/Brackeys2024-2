935291553cf01af59c128d69b89876a355fd4059
Ivan Diaz <impstudio2024@gmail.com>
2024-09-15 11:12:25 +0200

    FEAT: levels added to sceneRepo

--- brackeys-2024-2/scene_repository.gd
+++ brackeys-2024-2/scene_repository.gd
@@ -7,6 +7,12 @@ extends Node
 @export var level2Scene : PackedScene
 @export var level3Scene : PackedScene
 @export var level4Scene : PackedScene
+@export var level5Scene : PackedScene
+@export var level6Scene : PackedScene
+@export var level7Scene : PackedScene
+@export var level8Scene : PackedScene
+@export var level9Scene : PackedScene
+@export var level10Scene : PackedScene
 
 
 var levels = {}
@@ -19,5 +25,12 @@ func packLevels():
 		1: level1Scene,
 		2: level2Scene,
 		3: level3Scene,
-		4: level4Scene
+		4: level4Scene,
+		5: level5Scene,
+		6: level6Scene,
+		7: level7Scene,
+		8: level8Scene,
+		9: level8Scene,
+		10: level8Scene,
+		
 	}
